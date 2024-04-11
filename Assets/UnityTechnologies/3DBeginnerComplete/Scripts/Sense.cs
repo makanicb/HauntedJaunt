ce74f11aa1f71bf4b0b6b93d330f5a99f198c04b
makanicb <makanicb25@gmail.com>
2024-04-10 19:53:39 -0700

    Created sensor that will detect ghosts in range to catch John Lemon.

---
+++ Assets/UnityTechnologies/3DBeginnerComplete/Scripts/Sense.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sense : MonoBehaviour
+{
+    //The tag to lookout for
+    public string lookout;
+
+    //When a collision occurs
+    private void OnTriggerEnter(Collider other)
+    {
+        //If the other object's tag matches lookout
+        if(other.CompareTag(lookout))
+        {
+            Debug.Log(lookout);
+        }
+    }
+}
