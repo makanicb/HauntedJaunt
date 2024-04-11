67f2a2f24dc32e6e24e255546378c90cf7e11fc5
makanicb <makanicb25@gmail.com>
2024-04-10 20:24:35 -0700

    Added sweat. John Lemon starts sweating when nearby to a ghost.

--- Assets/UnityTechnologies/3DBeginnerComplete/Scripts/Sense.cs
+++ Assets/UnityTechnologies/3DBeginnerComplete/Scripts/Sense.cs
@@ -6,6 +6,8 @@ public class Sense : MonoBehaviour
 {
     //The tag to lookout for
     public string lookout;
+    //The gameobject to toggle when a lookout is encountered
+    public GameObject toggle;
 
     //When a collision occurs
     private void OnTriggerEnter(Collider other)
@@ -13,7 +15,17 @@ public class Sense : MonoBehaviour
         //If the other object's tag matches lookout
         if(other.CompareTag(lookout))
         {
-            Debug.Log(lookout);
+            Debug.Log("Hello there");
+            toggle.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag(lookout))
+        {
+            Debug.Log("Bye-bye");
+            toggle.SetActive(false);
         }
     }
 }
